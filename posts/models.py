607b7ad9cf5afd3267a29725cebdb62215c72e8e
mat_mal <matvey_maljanov@rambler.ru>
2023-05-22 15:23:42 +0300

    !

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from datetime import datetime
 
@@ -5,4 +6,9 @@ from datetime import datetime
 class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.CharField(max_length=1000)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    image = models.ImageField(upload_to='img', default='default.jpeg')
+    date = models.DateTimeField(default=datetime.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm, authenticate
+from .forms import MyUserCreationForm
 from .models import Post
 
 
@@ -8,5 +13,38 @@ def index(request):
 
 
 def post(request, pk):
-    posts = Post.objects.get(id=pk)
+    posts = get_object_or_404(Post, id=pk)
     return render(request, 'post.html', {'posts': posts})
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', {'form': MyUserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password1'])
+            user.save()
+            login(request, user)  # Вход пользователя
+            return redirect('index')
+        else:
+            return render(request, 'login.html',
+                          {'form': MyUserCreationForm(), 'error': 'Passwords do not match'})
+
+
+def log_out(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('index')
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'form': AuthenticationForm()})
+    else:
+        try:
+            user = authenticate(username=request.POST['username'], password=request.POST['password'])
+            login(request, user)  # Вход пользователя
+            return redirect('index')
+        except AttributeError:
+            return render(request, 'signup.html',
+                          {'form': AuthenticationForm(), 'error': 'Password is not correct'})
